56f61474e10e7d3a7edcfdfc176965e8eab8d929
Shifupapi <shifupapi@Seans-MacBook-Air.local>
2023-04-04 00:33:09 -0400

    finalcode

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "4f9fae3a1405adf2dee3aaeffea1a8c0"
+#"bf1a3e39fdbcea25655fa0805ebbdbe9"
+
+# Geoapify API Key
+geoapify_key = "f336f37a5afc466ba2f2c8c1a4f2d627"
